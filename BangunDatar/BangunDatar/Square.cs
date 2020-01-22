bc12c0506c20a153c9561b86d49e1e1f466a08a0
agus <aguschoirul7@gmail.com>
2020-01-22 08:38:03 +0700

    First Commit

---
+++ BangunDatar/BangunDatar/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangunDatar
+{
+    public class Square
+    {
+        private double sisi; // pendeklarasian untuk variabel
+
+        public void setSide(Double side){
+            this.sisi = side; // metod untuk mengambil nilai sisi 
+        }
+         public Double luas () //fungsi luas 
+        {
+            return Math.Pow(this.sisi,2); // retrun untuk pengembalian niali 
+        }
+        public Double keliling ()
+        {
+            return this.sisi * sisi;
+        }
+    }
+}
